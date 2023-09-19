03c8539186c12a97e2e2b021eca04bd1091fcdd1
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-09-19 15:34:09 +0100

    Mention Lucia's original repo

--- src/data/word-shower-react.ts
+++ src/data/word-shower-react.ts
@@ -18,6 +18,9 @@ You then shout out words (with 4 or more letters) as they occur to you and
 the app uses speech recognition to process the words. If the word is recognised
 and can be spelt using the active letters, the Scrabble value of the word will be added
 to your score.
+
+This app is based on [this repo](https://github.com/luciavelasco/word-shower) by
+[Lucia Velasco](https://github.com/luciavelasco).
   `,
   cardImage: {
     path: "screenshots/word-shower-react/card-image.webp",
